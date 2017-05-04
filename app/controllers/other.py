0f3fdf8974175db674a5f3e6f10badb3bcd84eaf
Nick Humrich <nick.humrich@gmail.com>
2017-05-04 10:43:09 -0600

    Fixes to callback endpoint

--- app/controllers/other.py
+++ app/controllers/other.py
@@ -1,3 +1,5 @@
+import json
+
 from waspy import Request, ResponseError, Application
 
 
@@ -17,20 +19,31 @@ async def error(request: Request):
 async def callback(request: Request):
     body = request.json()
     service = body.get('service')
+    port = body.get('port')
     path = body.get('path')
     data = body.get('body')
     method = body.get('method')
+
+    if method is None:
+        raise ResponseError('No method provided', 400,
+                            reason="'method' is a required field")
 
     if path is None:
         raise ResponseError('No path provided', 400,
                             reason="'path' is a required field.")
     app: Application = request.app
     response = await app.client.make_request(method, service, path, data,
-                                             context=request)
+                                             context=request, port=port)
+
+    try:
+        body = response.json()
+    except json.JSONDecodeError:
+        body = response.body.decode()
+
     return {
         'result':
             {'status_code': response.status,
-             'body': response.body,
+             'body': body,
              'headers': response.headers,
              }
     }
